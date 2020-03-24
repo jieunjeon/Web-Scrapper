3abc140a7737fbdee17cdacc9178ff2ca310b375
Jieun Jeon <jieun@crocusenergy.com>
2020-03-24 01:05:05 -0700

    variables and constants

--- learnGo/main.go
+++ learnGo/main.go
@@ -3,5 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	var name string = "jieun"
+	shortName := "jieun"
+	const name2 string = "jieun2"
+
+	fmt.Println(name, shortName)
 	fmt.Println("Hello World!")
 }
