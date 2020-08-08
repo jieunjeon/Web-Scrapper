a71604470fff358eef21835947c4022408c4d100
Jieun Jeon <jieun@crocusenergy.com>
2020-08-07 23:25:52 -0700

    add status check for urls

--- URL Checker/main.go
+++ URL Checker/main.go
@@ -9,7 +9,8 @@ import (
 var errRequestFailed = errors.New("Request failed")
 
 func main() {
-	urls := []string {
+	var results = make(map[string]string)
+	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
 		"https://www.amazon.com/",
@@ -19,15 +20,26 @@ func main() {
 		"https://www.facebook.com/",
 		"https://www.instagram.com/",
 	}
+	results["cello"] = "Hello"
 	for _, url := range urls {
-		hitURL(url)
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
+		// hitURL(url)
 	}
 }
 
-func hitURL (url string) error {
+func hitURL(url string) error {
 	fmt.Println("Checking:", url)
 	resp, err := http.Get(url)
-	if err == nil || resp.StatusCode >= 400 {
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
 		return errRequestFailed
 	}
 	return nil
